1e70a1015ff91ed53102cf650c3ee3b4a9ef6bad
Rayline Cruz <153952595+Cruz-Rayline-BSIT32E3@users.noreply.github.com>
2024-03-02 15:06:59 +0800

    Delete PRELIM_E3_Assignment_CruzRayline_BSIT32E3 directory

--- PRELIM_E3_Assignment_CruzRayline_BSIT32E3/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace PRELIM_E3_Assignment_CruzRayline_BSIT32E3.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
